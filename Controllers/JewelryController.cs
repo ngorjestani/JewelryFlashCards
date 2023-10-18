66928c38f81ce1c58d43dedf25acb94f7092fed7
ngorjestani <nickg2794@hotmail.com>
2023-10-17 21:38:00 -0500

    Added AngleSharp

---
+++ Controllers/JewelryController.cs
@@ -0,0 +1,21 @@
+﻿using JewelryFlashCards.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JewelryFlashCards.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class JewelryController
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public JewelryController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<IActionResult> GetAnatometalJewelry()
+    {
+        
+    }
+}
---
+++ Models/JewelryFlashCard.cs
@@ -0,0 +1,10 @@
+﻿namespace JewelryFlashCards.Models;
+
+public class JewelryFlashCard
+{
+    public string Name { get; set; }
+    
+    public string Link { get; set; }
+
+    public string ImageLink { get; set; }
+}
