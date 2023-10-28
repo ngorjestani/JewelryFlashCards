dbd5e8409bac909dfd44018e4e5ded5f47ed800e
ngorjestani <nickg2794@hotmail.com>
2023-10-27 22:05:09 -0500

    Added anatometal endpoint

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JewelryFlashCards.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BaseController : ControllerBase
+{
+    protected readonly ILogger<BaseController> Logger;
+    
+    public BaseController(ILogger<BaseController> logger)
+    {
+        Logger = logger;
+    }
+}
--- Controllers/JewelryController.cs
+++ Controllers/JewelryController.cs
@@ -1,21 +1,36 @@
-﻿using JewelryFlashCards.Models;
+﻿using AngleSharp;
+using JewelryFlashCards.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JewelryFlashCards.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class JewelryController
+public class JewelryController : BaseController
 {
-    private readonly ILogger<WeatherForecastController> _logger;
+    public JewelryController(ILogger<JewelryController> logger)
+        : base(logger) { }
 
-    public JewelryController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
+    [HttpGet]
+    [Route("get-anatometal-jewelry")]
     public async Task<IActionResult> GetAnatometalJewelry()
     {
-        
+        var config = Configuration.Default.WithDefaultLoader();
+        var context = BrowsingContext.New(config);
+        var document = await context.OpenAsync(
+            "https://anatometal.com/jewelry/threaded-and-threadless-ends/"
+        );
+        var productElements = document.QuerySelectorAll(".col-product");
+        var jewelryList = productElements
+            .Select(
+                x =>
+                    new JewelryFlashCard
+                    {
+                        Name = x.QuerySelector("a h4")?.TextContent ?? "",
+                        Link = x.QuerySelector("a")?.GetAttribute("href") ?? "",
+                        ImageLink = x.QuerySelector("a img")?.GetAttribute("src") ?? ""
+                    }
+            )
+            .ToList();
+
+        return Ok(jewelryList);
     }
 }
